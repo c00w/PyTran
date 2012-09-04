7fa1348d8d3acefffdef80534cb9d4c895fa6d9b
Colin <ricec2@rpi.edu>
2012-09-04 12:12:13 -0400

    More elegent solution to ce problem

--- ce.py
+++ ce.py
@@ -3,27 +3,49 @@ from threading import Lock
 from collections import defaultdict
 from pprint import pprint
 
-ce = defaultdict(lambda: None) 
 lock = Lock()
 
-def good_base_ce(old_ce, new_ce):
-    for item in new_ce:
-        if new_ce[item] != old_ce[item]:
-            if old_ce[item] != ce[item]:
-                return False
+class CE():
+    index = 0
+    data = defaultdict(lambda: None) 
+    new_data = defaultdict(lambda: None) 
+
+    def __get__(self, key):
+        if key in self.new_data:
+            return self.new_data[value]
+        return self.data[key]
+
+    def __set__(self, key, value):
+        self.new_data[key] = value
+
+    def __delete__(self, key):
+        self.new_data[key] = None
+    
+    def commit(self):
+        self.data = new_data
+        self.new_data = defaultdict(lambda: None) 
+        index += 1
+
+    def changed(self):
+        return self.new_data.keys()
+
+ce = CE()
+
+def good_base_ce(new_ce):
+    for item in new_ce.changed():
+        if new_ce.data[item] != ce[item]:
+            return False
     return True
 
 def get():
     return copy(ce)
 
-def update(old_ce, new_ce):
-    # In some cases this is an incorrect result
-    # Here for rapid prototyping
-    if not good_base_ce(old_ce, new_ce):
+def update(new_ce):
+    if not good_base_ce(new_ce):
         return False
-    if old_ce == ce and new_ce != old_ce:
+    if new_ce.changed():
         with lock:
-            for x in new_ce.iterkeys():
-                if new_ce[x] != old_ce[x]:
+            for x in new_ce.new_data.iterkeys():
                     ce[x] = new_ce[x]
+            ce.commit()
     return True
